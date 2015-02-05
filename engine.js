1c244b97ac13873998604ccbba077912617b3d5a
Peter Rifel <pgrifel@gmail.com>
2015-02-05 12:26:03 -0800

    Adding logic for comparing against true outcomes.  I think this is mostly accurate? Dont have a real tourny bracket to compare.  Green represents correct winner predictions.  Red represents incorrect winner predictions.  Still a few kinks to work out.

--- engine.js
+++ engine.js
@@ -118,12 +118,12 @@ function setupInitialMatches() {
         }
     }
 }
+
 /*
  * Determine the winner of a matchup based on weight.
  * Return the team object for the winning team.
  * Tie breaker is the higher overall rank
  */
-
 function getWinner(team1, team2) {
     team1Total = 0;
     team2Total = 0;
@@ -142,6 +142,43 @@ function getWinner(team1, team2) {
         return parseInt(team1.Rank) < parseInt(team2.Rank) ? team1 : team2;
     }
     return team1Total > team2Total ? team1 : team2;
+}
+
+/*
+ * Determines which team actually won in the matchup.
+ * If the matchup hasnt occured yet, return null.
+ */
+function setTrueWinner(team1, team2, round, winner, team1Div, team2Div) {
+    if(team1['Games Won'] == team2['Games Won'] && round == team1['Games Won']) {
+        //return null;
+        $(team1Div).removeClass('incorrect').removeClass('correct');
+        $(team2Div).removeClass('incorrect').removeClass('correct');
+    } else {
+        if(team1['Games Won'] > team2['Games Won'] && team1 == winner && team1['Games Won'] >= round) {
+            $(team1Div).removeClass('incorrect').addClass('correct');
+        } else if (team1['Games Won'] < team2['Games Won'] && team2 == winner && team2['Games Won'] >= round) {
+            $(team2Div).removeClass('incorrect').addClass('correct');        
+        }
+        
+        if(team1['Games Won'] < round && team1 == winner) {
+            $(team1Div).addClass('incorrect').removeClass('correct');
+        }
+        if(team2['Games Won'] < round && team2 == winner) {
+            $(team2Div).addClass('incorrect').removeClass('correct');
+        }
+    }
+    //return (team1['Games Won'] > team2['Games Won']) ? team1 : team2;
+}
+
+/* 
+ * Utility function for converting game numbers in gameWinners object to rounds.
+ */
+function getRound(gameNumber) {
+    if(gameNumber >= 1 && gameNumber <= 8) return 1;
+    if(gameNumber >= 9 && gameNumber <= 12) return 2;
+    if(gameNumber >= 13 && gameNumber <= 14) return 3;
+    if(gameNumber == 15) return 4;
+    return null;
 }
 /*
  * Loop through the list of weights, calculating the relative values.
@@ -177,13 +214,18 @@ function submit() {
     }
     for (var matchupID in firstFours) {
         var winner = getWinner(firstFours[matchupID][0], firstFours[matchupID][1]);
+        var team1Div = '#matchup' + matchupID + ' > .team1';
+        var team2Div = '#matchup' + matchupID + ' > .team2';
+        setTrueWinner(firstFours[matchupID][0], firstFours[matchupID][1], 0, winner, team1Div, team2Div);
+        
         if (winner == firstFours[matchupID][0]) {
-            $('#matchup' + matchupID + ' > .team1').removeClass('loser').addClass('winner');
-            $('#matchup' + matchupID + ' > .team2').removeClass('winner').addClass('loser');
+            $(team1Div).removeClass('loser').addClass('winner');
+            $(team2Div).removeClass('winner').addClass('loser');
         } else {
-            $('#matchup' + matchupID + ' > .team1').removeClass('winner').addClass('loser');
-            $('#matchup' + matchupID + ' > .team2').removeClass('loser').addClass('winner');
+            $(team1Div).removeClass('winner').addClass('loser');
+            $(team2Div).removeClass('loser').addClass('winner');
         }
+
         bracketTeamsByRegionAndSeed[winner.Region][winner.stats.Seed] = winner;
         $('#' + regions[winner.Region].toLowerCase() + 'seed' + winner.stats.Seed).text('(' + winner.stats.Seed + ') ' + winner.Name);
         $('#FirstFour' + matchupID + 'Result').text("Winner: " + winner.Name);
@@ -201,15 +243,20 @@ function submit() {
         for (var seed = 1; seed < 9; seed++) {
             var high = currentRegion[seed];
             var low = currentRegion[17 - seed];
+            var highDiv = '#' + region + 'seed' + high.stats.Seed;
+            var lowDiv = '#' + region + 'seed' + low.stats.Seed;
+            
             bracketData.teams.push(['(' + high.stats.Seed + ') ' + high.Name, '(' + low.stats.Seed + ') ' + low.Name]);
             var winner = getWinner(high, low);
+            setTrueWinner(high, low, 1, winner, highDiv, lowDiv);
             gameWinners['game' + String(seed)] = winner;
             $('#' + region + 'seed' + winner.stats.Seed).removeClass('loser').addClass('winner');
             if (high == winner) {
-                $('#' + region + 'seed' + low.stats.Seed).removeClass('winner').addClass('loser');
+                $(lowDiv).removeClass('winner').addClass('loser');
             } else {
-                $('#' + region + 'seed' + high.stats.Seed).removeClass('winner').addClass('loser');
+                $(highDiv).removeClass('winner').addClass('loser');
             }
+            
             $('#' + region + 'game' + seed).text('(' + winner.stats.Seed + ') ' + winner.Name);
         }
         // Round of 32 through the Elite 8
@@ -217,15 +264,19 @@ function submit() {
         for (var game = 9; game < 16; game++) {
             var high = gameWinners['game' + String(game - gameDiff)];
             var low = gameWinners['game' + String(game + 1 - gameDiff)];
+            var highDiv = '#' + region + 'game' + String(game - gameDiff);
+            var lowDiv = '#' + region + 'game' + String(game + 1- gameDiff);
             var winner = getWinner(high, low);
+            console.log(high.Name + " " + low.Name + ": " + String(gameDiff));
+            setTrueWinner(high, low, getRound(game), winner, highDiv, lowDiv);
             gameWinners['game' + String(game)] = winner;
             $('#' + region + 'game' + game).text('(' + winner.stats.Seed + ') ' + winner.Name);
             if (high == winner) {
-                $('#' + region + 'game' + String(game - gameDiff)).removeClass('loser').addClass('winner');
-                $('#' + region + 'game' + String(game + 1 - gameDiff)).removeClass('winner').addClass('loser');
+                $(highDiv).removeClass('loser').addClass('winner');
+                $(lowDiv).removeClass('winner').addClass('loser');
             } else {
-                $('#' + region + 'game' + String(game - gameDiff)).removeClass('winner').addClass('loser');
-                $('#' + region + 'game' + String(game + 1 - gameDiff)).removeClass('loser').addClass('winner');
+                $(highDiv).removeClass('winner').addClass('loser');
+                $(lowDiv).removeClass('loser').addClass('winner');
             }
             gameDiff--;
         }
@@ -238,20 +289,24 @@ function submit() {
         var region1 = regionID;
         var region2 = regionID + 1;
         var team1 = gameWinnerRegions[region1].game15;
+        var team1Div = '#' + regions[region1].toLowerCase() + 'game15';
+        var team2Div = '#' + regions[region2].toLowerCase() + 'game15'
         var team2 = gameWinnerRegions[region2].game15;
         var winner = getWinner(team1, team2);
+        setTrueWinner(team1, team2, 5, winner, team1Div, team2Div);
         championship[sides[side]] = winner;
         $('#' + sides[side] + 'game').text('(' + winner.stats.Seed + ') ' + winner.Name);
         if (team1 == winner) {
-            $('#' + regions[region1].toLowerCase() + 'game15').removeClass('loser').addClass('winner');
-            $('#' + regions[region2].toLowerCase() + 'game15').removeClass('winner').addClass('loser');
+            $(team1Div).removeClass('loser').addClass('winner');
+            $(team2Div).removeClass('winner').addClass('loser');
         } else {
-            $('#' + regions[region1].toLowerCase() + 'game15').removeClass('winner').addClass('loser');
-            $('#' + regions[region2].toLowerCase() + 'game15').removeClass('loser').addClass('winner');
+            $(team1Div).removeClass('winner').addClass('loser');
+            $(team2Div).removeClass('loser').addClass('winner');
         }
         regionID += 2;
     }
     var winner = getWinner(championship.left, championship.right);
+    setTrueWinner(championship.left, championship.right, 6, winner, '#leftgame', '#rightgame');
     $('#championship').text('(' + winner.stats.Seed + ') ' + winner.Name);
     if (championship.left == winner) {
         $('#leftgame').removeClass('loser').addClass('winner');
